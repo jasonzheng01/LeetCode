d35d11f102207125eefa28aef476c6a53a603567
jasonzheng01 <94309018+jasonzheng01@users.noreply.github.com>
2023-05-20 17:51:46 -0400

    Create 0021_MergeTwoSortedLists.py

---
+++ 0021_MergeTwoSortedLists.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        if not list1 or not list2:
+            return list1 or list2
+        dummy = ListNode(0)
+        curr = dummy
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+        curr.next = list1 or list2
+        return dummy.next
